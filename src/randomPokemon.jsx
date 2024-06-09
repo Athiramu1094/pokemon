47afe5fa2266b24bfb2c8dd6330696ea7682264b
Athira MU <krishna6athi@gmail.com>
2024-06-09 08:50:18 +0530

    updated

--- src/randomPokemon.jsx
+++ src/randomPokemon.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './randomPokemon.css'
 
 const RandomPokemon = () => {
+    const [num, setNum] = useState(69);
+    const [url, setUrl] = useState(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${num}.png`);
 
-    const pokeNum = Math.floor(Math.random()*151)+1
-    const url =  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeNum}.png`
-        return (
-    <div className='randomPokemon'>
-        <h1>Pokemon # {pokeNum}</h1>
-        <img src={url} alt=''/>
-    </div>
-)
+    function changeNumber() {
+        const pokeNum = Math.floor(Math.random() * 151) + 1;
+        const newUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeNum}.png`;
+        setNum(pokeNum);
+        setUrl(newUrl);
+    }
+
+    return (
+        <div className='randomPokemon'>
+            <h1>Pokemon # {num}</h1>
+            <img src={url} alt='Random Pokemon'/>
+            <button onClick={changeNumber}>Change Pokemon</button>
+        </div>
+    );
 }
+
 
 export default RandomPokemon
